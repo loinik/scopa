4cf1c733844bcc2a08b9bff1394383a30076035d
Mikel Lucyšyn <loinik@me.com>
2026-03-20 10:55:49 +0500

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+import manifest from './public/manifest.webmanifest';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/scopa/'
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest,
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,png,svg}'],
+            },
+        })
+    ],
+    base: '/scopa/'
 });
