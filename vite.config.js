437b48d90acb1c77d8476f4c67c5f236ae9ebc62
Mikel Lucyšyn <loinik@me.com>
2026-03-20 10:57:23 +0500

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-
-import manifest from './public/manifest.webmanifest';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     plugins: [
